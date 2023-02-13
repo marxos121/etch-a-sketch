62086d309eaf6adae689f6d3a450bfba3df7090f
marxos121 <marxos121@gmail.com>
2023-02-14 00:43:40 +0100

    declare button event as function

--- scripts.js
+++ scripts.js
@@ -1,53 +1,50 @@
-const body = document.querySelector("body");
-const grid = document.querySelector(".grid-container");
-
-function generateGrid(rowsAndColumns, size)
-{
+function generateGrid(rowsAndColumns, sizeInPixels) {
     for(let i = 0; i < rowsAndColumns * rowsAndColumns; ++i)
     {
         let cell = document.createElement("div");
         cell.classList.add("grid-cell");
         grid.appendChild(cell);
     }
-    let style = `${size}px` + ` ${size}px`.repeat(rowsAndColumns - 1);
+    let style = `${sizeInPixels}px` + ` ${sizeInPixels}px`.repeat(rowsAndColumns - 1);
     grid.style.gridTemplateColumns = style;
     grid.style.gridTemplateRows = style;
 }
 
-generateGrid(16, 35);
-
-
-const cells = document.querySelectorAll(".grid-cell");
-cells.forEach((cell) => {
-    let r = Math.floor(Math.random() * 1000) % 256;
-    let g = Math.floor(Math.random() * 1000) % 256;
-    let b = Math.floor(Math.random() * 1000) % 256;
-
-    cell.addEventListener("mouseover", () => {
-        cell.style.backgroundColor = `rgb(${r},${g},${b})`;
+function initializeHoverEvent() {
+    const cells = document.querySelectorAll(".grid-cell");
+    cells.forEach((cell) => {
+        let r = Math.floor(Math.random() * 1000) % 256;
+        let g = Math.floor(Math.random() * 1000) % 256;
+        let b = Math.floor(Math.random() * 1000) % 256;
+    
+        cell.addEventListener("mouseover", () => {
+            cell.style.backgroundColor = `rgb(${r},${g},${b})`;
+        });
     });
-});
+}
 
-
-const button = document.querySelector("button");
-button.addEventListener("click", () => {
+function setupNewGrid() {
     let input = parseInt(prompt("Enter new grid size."));
-    if(input <= 0) { return; }
+    if(isNaN(input) || input <= 0) { return; }
     if(input > 100) { input = 100; }
-    
-    let totalWidth = grid.style.width;
-    let totalHeight = grid.style.height;
-
-    let newGrid = document.createElement("div");
-    newGrid.classList.add("grid-container");
-    
-    let cellWidth = totalWidth / input;
-    let cellHeight = totalHeight / input;
-    newGrid.style.gridTemplateColumns = cellWidth + ` ${cellWidth}px`.repeat(input - 1);
-    newGrid.style.gridTemplateRows = cellHeight + ` ${cellHeight}px`.repeat(input - 1);
 
 
     body.removeChild(grid);
+    let newGrid = document.createElement("div");
+    newGrid.classList.add("grid-container");
     grid = newGrid;
     body.appendChild(grid);
-})
+
+    generateGrid(input, gridSize / input);
+    initializeHoverEvent();
+}
+
+const button = document.querySelector("button");
+button.addEventListener("click", setupNewGrid);
+
+const body = document.querySelector("body");
+let grid = document.querySelector(".grid-container");
+let initialColumns = 16, initialPixels = 35;
+const gridSize = initialColumns * initialPixels;
+generateGrid(initialColumns, initialPixels);
+initializeHoverEvent();
