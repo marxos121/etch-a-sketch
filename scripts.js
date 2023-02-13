7744e161846f7e4b5118a2431dda23b27c5dfdbb
marxos121 <marxos121@gmail.com>
2023-02-13 23:30:13 +0100

    change to random background colour on hover

--- scripts.js
+++ scripts.js
@@ -7,3 +7,14 @@ for(let i = 0; i < 16 * 16; ++i)
 }
 let style = "1fr" + " 1fr".repeat(15);
 grid.style.gridTemplateColumns = style;
+
+const cells = document.querySelectorAll(".grid-cell");
+cells.forEach((cell) => {
+    let r = Math.floor(Math.random() * 1000) % 256;
+    let g = Math.floor(Math.random() * 1000) % 256;
+    let b = Math.floor(Math.random() * 1000) % 256;
+
+    cell.addEventListener("mouseover", () => {
+        cell.style.backgroundColor = `rgb(${r},${g},${b})`;
+    });
+});
