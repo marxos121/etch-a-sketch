de64c5574c329cc85c5a09b61e763a0277935a11
marxos121 <marxos121@gmail.com>
2023-02-14 01:48:24 +0100

    add vanishing of color

--- scripts.js
+++ scripts.js
@@ -10,16 +10,30 @@ function generateGrid(rowsAndColumns, sizeInPixels) {
     grid.style.gridTemplateRows = style;
 }
 
-function initializeHoverEvent() {
-    const cells = document.querySelectorAll(".grid-cell");
-    cells.forEach((cell) => {
+function setCellColor(e) {
+    let color = window.getComputedStyle(this).getPropertyValue("background-color");
+    if(color === "rgb(0, 0, 0)"){
         let r = Math.floor(Math.random() * 1000) % 256;
         let g = Math.floor(Math.random() * 1000) % 256;
         let b = Math.floor(Math.random() * 1000) % 256;
-    
-        cell.addEventListener("mouseover", () => {
-            cell.style.backgroundColor = `rgb(${r},${g},${b})`;
-        });
+        this.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+        this.setAttribute("diminish-r", `${Math.ceil(r * 0.1)}`);
+        this.setAttribute("diminish-g", `${Math.ceil(g * 0.1)}`);
+        this.setAttribute("diminish-b", `${Math.ceil(b * 0.1)}`);
+    }
+    else {
+        let currentRGB = color.replace(/[^\d,]/g, '').split(',');
+        let newR = +currentRGB[0] - +this.getAttribute("diminish-r") > 0 ? +currentRGB[0] - +this.getAttribute("diminish-r") : 0;
+        let newG = +currentRGB[1] - +this.getAttribute("diminish-g") > 0 ? +currentRGB[1] - +this.getAttribute("diminish-g") : 0;
+        let newB = +currentRGB[2] - +this.getAttribute("diminish-b") > 0 ? +currentRGB[2] - +this.getAttribute("diminish-b") : 0;
+        this.style.backgroundColor = `rgb(${newR}, ${newG}, ${newB})`;
+    }
+}
+
+function initializeHoverEvent() {
+    const cells = document.querySelectorAll(".grid-cell");
+    cells.forEach((cell) => {  
+        cell.addEventListener("mouseover", setCellColor);
     });
 }
 
